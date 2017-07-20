9261aa539d1664e72f0c0143645a91f16967ac5e
caitlinhgray <caitlingray@caitlinsmacbook.lan>
2017-07-20 11:26:28 -0700

    athlete bios
    All of the jQuery stuff for the athlete bios.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,80 @@
 // Make it rain!
+$(".bonica_text").hide();
 
-Pseudocode:
+$(".bonica").on("mouseover", function(){
+  $(".bonica_text").show();
+});
+
+$(".bonica").on("mouseout", function(){
+  $(".bonica_text").hide();
+});
+
+$(".jen_text").hide();
+
+$(".jen").on("mouseover", function(){
+  $(".jen_text").show();
+});
+
+$(".jen").on("mouseout", function(){
+  $(".jen_text").hide();
+});
+
+$(".katie_text").hide();
+
+$(".katie").on("mouseover", function(){
+  $(".katie_text").show();
+});
+
+$(".katie").on("mouseout", function(){
+  $(".katie_text").hide();
+});
+
+$(".paige_text").hide();
+
+$(".paige").on("mouseover", function(){
+  $(".paige_text").show();
+});
+
+$(".paige").on("mouseout", function(){
+  $(".paige_text").hide();
+});
+
+$(".kimberly_text").hide();
+
+$(".kimberly").on("mouseover", function(){
+  $(".kimberly_text").show();
+});
+
+$(".kimberly").on("mouseout", function(){
+  $(".kimberly_text").hide();
+});
+
+$(".liz_text").hide();
+
+$(".liz").on("mouseover", function(){
+  $(".liz_text").show();
+});
+
+$(".liz").on("mouseout", function(){
+  $(".liz_text").hide();
+});
+
+$(".marisa_text").hide();
+
+$(".marisa").on("mouseover", function(){
+  $(".marisa_text").show();
+});
+
+$(".marisa").on("mouseout", function(){
+  $(".marisa_text").hide();
+});
+
+$(".meg_text").hide();
+
+$(".meg").on("mouseover", function(){
+  $(".meg_text").show();
+});
+
+$(".meg").on("mouseout", function(){
+  $(".meg_text").hide();
+});
